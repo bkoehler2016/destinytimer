51e92202f3a48a234c69d93b79502d5afacefdbd
Benjamin Koehler <bkoehler751@gmail.com>
2022-03-13 16:40:27 -0700

    updated time

--- src/Componets/Clock.js
+++ src/Componets/Clock.js
@@ -11,7 +11,7 @@ function DestinyTime() {
       <ul>
         <li>Destiny Time 10:00 AM</li>
         <li>East Coast 1:00 PM</li>
-         <li>UK 6:00 PM</li>
+        <li>UK 6:00 PM</li>
         <li>Europe 7:00 PM</li>
       </ul>
       <Row>
